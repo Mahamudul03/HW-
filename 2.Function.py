19e76e74b355680dbb1efd2d3f03fdc852de6a9f
Han118312 <124232328+Mahamudul03@users.noreply.github.com>
2025-01-08 15:02:39 +0600

    Update 2.Function.py

--- 2.Function.py
+++ 2.Function.py
@@ -1,6 +1,9 @@
-
-
-numbers_list = [1, 2, 3, 4, 5, 6, 7,8,9]
+# Sum even and odd numbers 
+def sum_even_odd(numbers):
+    even_sum = sum(n for n in numbers if n % 2 == 0)
+    odd_sum = sum(n for n in numbers if n % 2 != 0)
+    return even_sum, odd_sum
+numbers_list = [1, 2, 3, 4, 5, 6]
 even_sum, odd_sum = sum_even_odd(numbers_list)
 print("Sum of even numbers:", even_sum)
 print("Sum of odd numbers:", odd_sum)

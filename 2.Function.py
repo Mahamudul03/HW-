a23f59897253032cb85ec33460324cb597c08520
Han118312 <124232328+Mahamudul03@users.noreply.github.com>
2025-01-08 14:56:33 +0600

    Update 2.Function.py

--- 2.Function.py
+++ 2.Function.py
@@ -1,9 +1,5 @@
-# Odd and Even :
-    even_sum: int = sum(n for n in numbers if n % 2 == 0)
-    odd_sum = sum(n for n in numbers if n % 2 != 0)
-    return even_sum, odd_sum
 
-# Example usage
+
 numbers_list = [1, 2, 3, 4, 5, 6, 7,8,9]
 even_sum, odd_sum = sum_even_odd(numbers_list)
 print("Sum of even numbers:", even_sum)
